4094dacfa31e918f89d3aefaed6ae34f02a081ac
ymizushi <mizushi@gmail.com>
2020-06-10 07:10:46 +0900

    edit code

--- app/javascript/packs/top_react.tsx
+++ app/javascript/packs/top_react.tsx
@@ -1,7 +1,12 @@
-import * as React from 'react'
-import * as ReactDOM from 'react-dom'
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+import * as Modal from 'react-modal';
 
 import Circle from './components/charts/circle';
+
+import store from './store/sample';
+
+ Modal.setAppElement('#modal');
 
 const data = {
   name: "１日の活動時間",
@@ -13,8 +18,36 @@ const data = {
   total: 24 * 60
 }
 
-const App = ({name: string}) => (
+
+const App = ({name: string}) => {
+  var subtitle;
+  const [modalIsOpen,setIsOpen] = React.useState(false);
+  function openModal() {
+    setIsOpen(true);
+  }
+  
+  function afterOpenModal() {
+    // references are now sync'd and can be accessed.
+  }
+  
+  function closeModal() {
+    setIsOpen(false);
+  }
+
+  const click = (e) => {
+    store.dispatch({ type: 'INCREMENT' })
+    setIsOpen(true);
+  }
+  return (
   <>
+    <Modal
+      isOpen={modalIsOpen}
+      onAfterOpen={afterOpenModal}
+      onRequestClose={closeModal}
+      contentLabel="Example Modal"
+    >
+    hoge
+   </Modal>
     <Circle r={200} data={data} />
     <table>
       <tr>
@@ -31,12 +64,15 @@ const App = ({name: string}) => (
         )
       }
     </table>
+    <div id="modal-point"></div>
+    <button onClick={click}>click</button>
   </>
 )
+}
 
 document.addEventListener('DOMContentLoaded', () => {
   ReactDOM.render(
-    <App name="sibyl" />,
+    <App id="app" name="sibyl" />,
     document.body.appendChild(document.createElement('div')),
   )
 })
